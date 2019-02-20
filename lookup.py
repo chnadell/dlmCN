c13fb03d4f5a02cf2a31d786eb26be510453b6a9
chnadell <christian.nadell@duke.edu>
2019-02-20 09:21:47 -0500

    Progress on lookup.py.

--- lookup.py
+++ lookup.py
@@ -1,23 +1,33 @@
 import os
 
 def gridGen(data_dir, grid_dir):
+
     # get data to be read
-    file_paths = os.listdir(data_dir)
+    file_paths = [os.path.join(data_dir, file) for file in os.listdir(data_dir)]
 
     # define output file
     outFile = os.path.join(grid_dir, 'gridDataOut.txt')
 
-    # open files
-    with open(outFile) as f_out:
-        for file_path in file_paths:
-            with open(file_path, 'r') as f:
-                # read the data
-                line = f.readline()
+    # define data generator
+    def get_geom(file_path):
+        with open(file_path) as file:
+            line = file.readline()
+            yield line
 
-                # write the data to predictions file
+    # open files and get geometry
+    with open(os.path.join(grid_dir, 'gridFile.csv'), 'w+') as gridFile:
+        for file_path in file_paths:
+            print('file path is {}'.format(file_path))
+            iter_lines = iter(get_geom(file_path))
+            for line in iter_lines:
+                geom = line[:8]
+                gridFile.write(geom)
+
+
+
 
 
 
 
 if __name__=="__main__":
-    gridGen(os.path.join('.','dataIn','eval'))
+    gridGen(os.path.join('.','dataIn','eval'), os.path.join('.', 'dataGrid'))
